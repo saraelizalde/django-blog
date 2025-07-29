708270d8c814d0e0e840e7834076fca380bd7a7e
saraelizalde <j7esg480@students.codeinstitute.net>
2025-07-29 12:27:47 +0100

    Add comment CRUD

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,8 @@ from django.urls import path
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('post/<slug:slug>/', views.post_detail, name='post_detail'),
+    path('post/<slug:slug>/edit_comment/<int:comment_id>',
+         views.comment_edit, name='comment_edit'),
+    path('post/<slug:slug>/delete_comment/<int:comment_id>',
+         views.comment_delete, name='comment_delete'),
 ]
