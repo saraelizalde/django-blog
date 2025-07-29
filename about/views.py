87c100f084d0783480ef81674dcc2a52d3da2de5
saraelizalde <j7esg480@students.codeinstitute.net>
2025-07-29 11:41:46 +0100

    Create form in about

--- about/views.py
+++ about/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import About
+from .forms import CollaborateForm
 
 # Create your views here.
 
@@ -7,9 +8,14 @@ def about_view(request):
     """
     Display the latest About page content.
     """
-    about = About.objects.order_by('-updated_at').first()
+    about = About.objects.order_by('-updated_on').first()
+    collaborate_form = CollaborateForm()
 
     return render(
-        request, 
-        "about/about.html", 
-        {'about': about})
+        request,
+        "about/about.html",
+        {
+            "about": about,
+            "collaborate_form": collaborate_form
+        },
+    )
